5beab9b89336e20ab49197985226e7b7ea6c6d52
Kingsley <kingobaino@gmail.com>
2021-02-04 18:12:42 +0100

    Select html elements

---
+++ js/library.js
@@ -0,0 +1,9 @@
+let title = document.querySelector('#title');
+let author = document.querySelector('#author');
+let pages = document.querySelector('#page')
+let read = document.querySelector('#read')
+let btn = document.querySelector('#btn');
+let add = document.querySelector('#add-book');
+let submit = document.querySelector('#submit');
+let row = document.querySelector('.row')
+
